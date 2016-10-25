4a160042b198324412d6082a65df8144b7e5b583
richard_dharmadi <s1567619@sms.ed.ac.uk>
2016-10-25 12:32:54 +0100

    task2

---
+++ assignment1/reducer_un.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import sys
+
+prev_sentence = ""
+value_total = 0
+sentence = ""
+
+for line in sys.stdin:          # For ever line in the input from stdin
+    line = line.strip("\n")         # Remove trailing characters
+    sentence, value = line.split("\t", 1)
+    value = int(value)
+    # Remember that Hadoop sorts map output by key reducer takes these keys sorted
+    if prev_sentence == sentence:
+        value_total += value
+    else:
+        if (prev_sentence) and (value_total == 1):  # write result to stdout
+            print("{0}\t{1}".format(prev_sentence, value_total))
+            
+        value_total = value
+        prev_sentence = sentence
+
+if (prev_sentence == sentence)and(value_total == 1):  # Don't forget the last key/value pair
+    print("{0}\t{1}".format(prev_sentence, value_total))
